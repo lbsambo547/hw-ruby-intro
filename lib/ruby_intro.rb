91707571b64394aa679aa596c8a535dfa343e412
lbsambo547 <samuelbrokaw@gmail.com>
2015-09-11 00:59:04 +0000

    finished part 1 and part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,43 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+   sum = 0
+    arr.each do |n|
+        sum = sum + n
+    end
+    return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.size == 0
+    return arr[0] if arr.size == 1
+    arr.sort!
+    arr.reverse!
+    sum = 0
+    sum = arr[0] + arr[1]
+    return sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.size == 0
+    return false if arr.size == 1
+    !!arr.uniq.combination(2).detect { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[bcdfghjklmnpqrstvwxyz]/i === s
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s.empty? || s =~ /[^01]/
+  binary = /^[1|0]+/.match(s)
+	return true if binary.to_s.to_i(2) % 4 == 0
 end
 
 # Part 3
